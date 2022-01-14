a75fbd33fb9bb2b640da813fd15c9568a18ab76e
Melnyk Andriy <a_melnica@yahoo.com>
2022-01-14 12:36:07 +0200

    add unit 3,checkout slice usage

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {pokemon.map(pokemon => (
+          {pokemon.slice(0, 20).map(pokemon => (
             <tr key={pokemon.id}>
               <td>{pokemon.name.english}</td>
               <td>{pokemon.type.join(', ')}</td>
